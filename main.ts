ce49f06a9428093ca126c7dd806f42e5c16a15f6
kodai1004 <e1924086@oit.ac.jp>
2024-10-16 18:08:14 +0900

    MakeCodeプロジェクトの初期ファイル

---
+++ main.ts
@@ -0,0 +1,15 @@
+input.onButtonPressed(Button.A, function () {
+    while (true) {
+        basic.showIcon(IconNames.Yes)
+        basic.showIcon(IconNames.No)
+        basic.showIcon(IconNames.Happy)
+        basic.showIcon(IconNames.Square)
+        basic.showIcon(IconNames.Heart)
+    }
+})
+loops.everyInterval(500, function () {
+	
+})
+basic.forever(function () {
+	
+})
